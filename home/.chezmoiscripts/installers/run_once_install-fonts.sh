6fee1b2ac3db2fd0cf56d958c5f16d6d91351db7
Fabio Rehm <fgrehm@gmail.com>
2025-05-31 08:31:37 -0300

    nvim configs (lazyvim starter template + required packages), terminal font

--- home/.chezmoiscripts/installers/run_once_install-fonts.sh
+++ home/.chezmoiscripts/installers/run_once_install-fonts.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+set -eo pipefail
+
+# TODO: Convert this to `.chezmoiexternal` format
+
 # https://github.com/basecamp/omakub/blob/master/install/desktop/fonts.sh
+
+mkdir -p ~/.local/share/fonts
 
 cd /tmp
 wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaMono.zip
@@ -13,5 +19,5 @@ unzip iafonts.zip -d iaFonts
 cp iaFonts/iA-Fonts-master/iA\ Writer\ Mono/Static/iAWriterMonoS-*.ttf ~/.local/share/fonts
 rm -rf iafonts.zip iaFonts
 
-fc-cache
+fc-cache -fv
 cd -
