9eee9d3c6c675b5e0f4c3a62f3a1c2fdf3bdf1fd
Fabio Rehm <fgrehm@gmail.com>
2025-06-03 09:47:48 -0300

    nvim: Auto save and tmux arrow navigation

--- configs/nvim/lua/config/keymaps.lua
+++ configs/nvim/lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- vim + tmux goodies
+-- https://github.com/alexghergh/nvim-tmux-navigation
+local nvim_tmux_nav = require("nvim-tmux-navigation")
+vim.keymap.set("n", "<A-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+vim.keymap.set("n", "<A-Left>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+vim.keymap.set("n", "<A-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+vim.keymap.set("n", "<A-Down>", nvim_tmux_nav.NvimTmuxNavigateDown)
+vim.keymap.set("n", "<A-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+vim.keymap.set("n", "<A-Up>", nvim_tmux_nav.NvimTmuxNavigateUp)
+vim.keymap.set("n", "<A-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+vim.keymap.set("n", "<A-Right>", nvim_tmux_nav.NvimTmuxNavigateRight)
+vim.keymap.set("n", "<A-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+vim.keymap.set("n", "<A-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
---
+++ configs/nvim/lua/plugins/auto-save.lua
@@ -0,0 +1,7 @@
+-- Autosave coz I'm lazy
+return {
+  {
+    "Pocco81/auto-save.nvim",
+    event = "VeryLazy",
+  },
+}
